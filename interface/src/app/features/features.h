b80af034527105dbef9aa47d2ed10210491edcf9
KarimWalidFawzy <kwasaq.0183@gmail.com>
2026-01-02 18:10:37 +0200

    .

--- interface/src/app/features/features.h
+++ interface/src/app/features/features.h
@@ -1,12 +1,5 @@
 #ifndef FEATURES_H
 #define FEATURES_H
- #include <Arduino.h>
- #include <TensorFlowLite_ESP32.h>
- #include "tensorflow/lite/builtin_ops.h"
- #include "tensorflow/lite/micro/kernels/micro_ops.h"
- #include "tensorflow/lite/micro/micro_error_reporter.h"
- #include "tensorflow/lite/micro/micro_interpreter.h"
- #include "tensorflow/lite/micro/all_ops_resolver.h"
  #include "../inference/model_data.h"
  /*
 
--- interface/src/app/inference/model_data.h
+++ interface/src/app/inference/model_data.h
@@ -1,6 +1,12 @@
 #ifndef MODEL_DATA_H
 #define MODEL_DATA_H
-
+    #include <Arduino.h>
+    #include <TensorFlowLite_ESP32.h>
+    #include "tensorflow/lite/builtin_ops.h"
+    #include "tensorflow/lite/micro/kernels/micro_ops.h"
+    #include "tensorflow/lite/micro/micro_error_reporter.h"
+    #include "tensorflow/lite/micro/micro_interpreter.h"
+    #include "tensorflow/lite/micro/all_ops_resolver.h"
     unsigned char g_model_data[] = 
     {
     // TFLite model bytes go here (from Python conversion)
