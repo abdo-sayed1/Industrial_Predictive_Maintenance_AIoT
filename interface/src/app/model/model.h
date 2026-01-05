a2c6ca34e63742ef8074ec8022941db969d83715
KarimWalidFawzy <kwasaq.0183@gmail.com>
2026-01-05 20:06:59 +0200

    Updated files to use quantized motor fault model and included health score in the model cpp file from mariam's branch.

--- interface/src/app/model/model.h
+++ interface/src/app/model/model.h
@@ -4,5 +4,7 @@
     const uint8_t* getfaultmodel();
     const unsigned short int getfaultmodellength();
     const char** getfaultlabels();
+    const float* getscalermean();
+    const float* getscalerstd();
     #include "../../../src/config.h"
 #endif 
